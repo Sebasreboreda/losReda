61042f07fa9aa8c79a3c71f961a91f77df5f908c
SebasReboreda <sebasoteroreboreda69@gmail.com>
2026-04-07 18:26:15 +0200

    ignorar
    si se sube al repositorio de tfg ignorar

--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,14 @@
 const path = require('path');
 const { Sequelize } = require('sequelize');
 
+const isPkg = typeof process.pkg !== 'undefined';
+const defaultDbPath = isPkg
+  ? path.join(path.dirname(process.execPath), 'database.sqlite')
+  : path.join(__dirname, '../../database.sqlite');
+
 const storage = process.env.DATABASE_PATH
   ? path.resolve(process.env.DATABASE_PATH)
-  : path.join(__dirname, '../../database.sqlite');
+  : defaultDbPath;
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
